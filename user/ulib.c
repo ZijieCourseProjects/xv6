4859995442a1435fc4a6277f136bd1de9dad89d2
3020202295-ZHAOZIJIE <veweew@tju.edu.cn>
2021-10-18 15:33:20 +0800

    Change the include path

--- user/ulib.c
+++ user/ulib.c
@@ -1,7 +1,7 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "kernel/fcntl.h"
-#include "user/user.h"
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../kernel/fcntl.h"
+#include "../user/user.h"
 
 char*
 strcpy(char *s, const char *t)
--- user/wc.c
+++ user/wc.c
@@ -1,6 +1,6 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "user/user.h"
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../user/user.h"
 
 char buf[512];
 
